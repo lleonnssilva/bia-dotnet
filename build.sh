80c1c00bc8be21446471d376adcbf3ba2416a712
25215344809 <leoguaruleo@gmail.com>
2025-01-15 12:32:02 -0300

    Primeiro Commit EB

---
+++ build.sh
@@ -0,0 +1,10 @@
+versao=$(git rev-parse HEAD | cut -c 1-7)
+aws ecr get-login-password --region us-east-1 --profile bia | docker login --username AWS --password-stdin 211125447912.dkr.ecr.us-east-1.amazonaws.com/bia
+docker build -t bia .
+docker tag bia:latest 211125447912.dkr.ecr.us-east-1.amazonaws.com/bia:$versao
+docker push 211125447912.dkr.ecr.us-east-1.amazonaws.com/bia:$versao
+rm .env 2> /dev/null
+./gerar-compose.sh
+rm bia-versao-*zip
+zip -r bia-versao-$versao.zip docker-compose.yml
+git checkout docker-compose.ymls
