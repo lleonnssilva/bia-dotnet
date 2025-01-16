22914091c48c28f23c2166ad389044e0094f0fc3
25215344809 <leoguaruleo@gmail.com>
2025-01-16 12:25:25 -0300

    ajuste conexão

--- Program.cs
+++ Program.cs
@@ -3,10 +3,19 @@ using BIA.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Configuration.AddEnvironmentVariables();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<MeuDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+var user = builder.Configuration["DB_USER"];
+var password = builder.Configuration["DB_PWD"];
+var host = builder.Configuration["DB_HOST"];
+var port = builder.Configuration["DB_PORT"];
+var db = builder.Configuration["DB"];
+
+string conectionstring =  String.Format("User ID={0};Password={1};Server={2};Port={3};Database={4}; Pooling=true;", user, password,host,port,db);
+Console.WriteLine($"*************conectionstring**************:\t{conectionstring}");
+builder.Services.AddDbContext<MeuDbContext>(options => options.UseNpgsql(conectionstring));
+// builder.Services.AddDbContext<MeuDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddTransient<IServiceApi, ServiceApi>();
 var app = builder.Build();
 
